3ae94228372baebbbe6b1acc3030f1fe467db74f
hannahlf <bananz1106@gmail.com>
2021-03-11 11:06:59 -0700

    attempt 2

---
+++ utils/WeatherApiKey.js
@@ -0,0 +1,1 @@
+export const API_KEY= '888067c4094bec429127395fd3c3ba49'
---
+++ utils/WeatherConditions.js
@@ -0,0 +1,38 @@
+export const weatherConditions = {
+	Rain: {
+		color: '#005BEA',
+		title: 'Raining',
+		subtitle: 'Not so nice',
+	},
+	Clear: {
+		color: '#f7b733',
+		title: 'So Sunny',
+		subtitle: 'Watch for warming',
+	},
+	Thunderstorm: {
+		color: '#616161',
+		title: 'A Storm is coming',
+		subtitle: 'Shouldnt be out here probably',
+	},
+	Clouds: {
+		color: '#1F1C2C',
+		title: 'Clouds',
+		subtitle: 'Everywhere, might be nice',
+	},
+
+	Snow: {
+		color: '#00d2ff',
+		title: 'Snow',
+		subtitle: 'Best thing ever',
+	},
+	Drizzle: {
+		color: '#076585',
+		title: 'Drizzle',
+		subtitle: 'Partially raining...',
+	},
+	Haze: {
+		color: '#66A6FF',
+		title: 'Haze',
+		subtitle: 'At least in the mountains its not straight pollution',
+	}
+};
